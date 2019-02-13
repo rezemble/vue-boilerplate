6ddf4228a74b430d1d7c885b8705ebac77ec7715
Lix Luthor <felix.schaetzler@ima-zlw-ifu.rwth-aachen.de>
2019-02-13 17:54:22 +0100

    remove helmet

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,3 @@
 module.exports = api => {
   api.render('./template');
-  api.injectImports(api.entryFile, `import { Helmet, HelmetProvider } from '@jnields/vue-helmet'`);
-  api.injectImports(api.entryFile, `Vue.component('Helmet', Helmet)`);
-  api.injectImports(api.entryFile, `Vue.component('HelmetProvider', HelmetProvider)`);
 };
